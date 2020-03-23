525308cb4eba24c6254257b95491406e4ed6e997
AshokSharma85 <ashoksharma8504@gmail.com>
2020-03-23 16:40:58 +0530

    Source Files

--- src/main/java/com/capgemini/service/ServiceClass.java
+++ src/main/java/com/capgemini/service/ServiceClass.java
@@ -54,25 +54,25 @@ public class ServiceClass implements IServiceClass  {
 	 * This method will check the username and password which is written by user after validation 
 	 * if data is in collection then it will return true other it will written false
 	*/
-@Override
-public boolean validate(int userName, String password) {
+	@Override
+	public boolean validate(int userName, String password) {
 	return objectOfCheckLoginUser.checkLogin(userName,password);
 	
-}
+	}
 	/*
 	 * For transferring money if receiver user is present is not it will give result according to this in boolean
 	*/
-@Override
-public boolean validateReceiver(int username) {
+	@Override
+	public boolean validateReceiver(int username) {
 
 	return objectOfCheckLoginUser.checkReceiverDao(username);
-}
-/*
- *This method will check whether user is trying to send the money to himself/herself or not	 
- */
-@Override
-public boolean validateUserAndRecieverService(int userNameOfSender, int userNameOfReceiver) {
+	}
+	/*
+	 *This method will check whether user is trying to send the money to himself/herself or not	 
+	 */
+	@Override
+	public boolean validateUserAndRecieverService(int userNameOfSender, int userNameOfReceiver) {
 	return objectOfCheckLoginUser.validateUserAndRecieverDao(userNameOfSender,userNameOfReceiver);
-}
+	}
 
 }
